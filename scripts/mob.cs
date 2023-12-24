871bb9b6eddc70607b5fc3d270ad942e8175a9e5
Cam McInnes <camtmcinnes@gmail.com>
2023-12-24 14:00:23 -0800

    fix hitbox

--- scripts/mob.cs
+++ scripts/mob.cs
@@ -6,6 +6,8 @@ public partial class mob : RigidBody2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		animatedSprite2D.Play();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
